0af458225e9e660899c70982172384ed1a021be1
monthack <54788688+monthack@users.noreply.github.com>
2022-09-18 19:45:49 -0500

    add increment , crement and reset

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import { CounterWithCustomHook } from './01-userState/CounterWithCustomHook';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <CounterWithCustomHook />
   </StrictMode>
 );
